a2e864fd66ac81574a29f15a2d85061d08ec735d
LeanneAugustin <160538347+LeanneAugustin@users.noreply.github.com>
2024-04-04 15:57:29 +0200

    Add files via upload

---
+++ ft_print_reverse_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leaugust <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/14 17:30:41 by leaugust          #+#    #+#             */
+/*   Updated: 2024/03/16 16:34:14 by leaugust         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void);
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	i;
+
+	i = 'z';
+	while (i >= 'a')
+	{
+		write(1, &i, 1);
+		i --;
+	}
+}
